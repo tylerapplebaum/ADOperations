6693eaa70b9d570bdfebec2f1abf086ccf6c57a6
Tyler Applebaum <tyler@us175.com>
2022-01-24 17:50:48 -0800

    Update Add-TestUsers.ps1
    Merged in last change from https://gist.github.com/tylerapplebaum/d692d9d2e1335b8b111927c8292c5dac
    
    All new changes will take place in the repo, not the gist.

--- Add-TestUsers.ps1
+++ Add-TestUsers.ps1
@@ -1,55 +1,55 @@
+
 <#
 .SYNOPSIS
 Create realistic-looking Active Directory accounts.
 Written by Tyler Applebaum.
-Version 0.1
-Last Updated Nov 19 2016
-
+Version 0.2
+Last Updated Jun 17 2020
 .LINK
 https://gist.github.com/tylerapplebaum/d692d9d2e1335b8b111927c8292c5dac
 https://randomuser.me/
-
 .DESCRIPTION
 Queries randomuser.me to generate user information. Creates an Active Directory user based on that.
-
 .PARAMETER NumUsers
 Specify the number of users to create
-
 .PARAMETER CompanyName
 Specify the company name to be used in the AD users' profile
-
 .PARAMETER Nationalities
 Specify the nationality of the users you are creating. randomuser.me relies on this for correct address formatting.
-
 .INPUTS
 System.String, System.Int32
-
 .OUTPUTS
 CSV with the creation results; Active Directory user account
-
 .EXAMPLE
-PS C:\> Add-TestUsers.ps1 -NumUsers 10
+PS C:\&gt; Add-TestUsers.ps1 -NumUsers 10
 Creates 10 AD user accounts
-
 .EXAMPLE
-PS C:\> Add-TestUsers.ps1 -NumUsers 18 -CompanyName "Apple Computer"
+PS C:\&gt; Add-TestUsers.ps1 -NumUsers 18 -CompanyName "Apple Computer"
 Creates 18 AD user accounts with Apple Computer as the Company Name under Organization
 #>
 
 [CmdletBinding()]
     param(
-	[Parameter(mandatory=$true, HelpMessage="Specify the number of users to create")]
-	[Alias("users")]
+    [Parameter(mandatory=$true, HelpMessage="Specify the number of users to create")]
+    [Alias("users")]
     [ValidateRange(1,1000)]
     [int]$NumUsers,
+
+    [Parameter(HelpMessage="Specify credentials to connect from a non-domain-joined computer")]
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $Credentials = [System.Management.Automation.PSCredential]::Empty,
+    
     [Parameter(HelpMessage="Specify the company name")]
     [Alias("co")]
     [string]$CompanyName = "Evil Corp",
+    
     [Parameter(HelpMessage="Specify the users' nationalities")]
     [Alias("nat")]
     [string]$Nationalities = "US"
-	)
-	
+    )
+    
 Function script:Set-Environment {
 $RandomUsersArr = New-Object System.Collections.ArrayList
 $Date = (Get-Date -Format (Get-Culture).DateTimeFormat.ShortDatePattern) -replace '/','.'
@@ -61,22 +61,19 @@ $DesktopPath = [Environment]::GetFolderPath("Desktop")
     Return $_.Exception.Message
   }
 
-  $DomainInfo = Get-ADDomain
+  $DomainInfo = Get-ADDomain -Credential $Credentials -Current LocalComputer
   $UsersOU=$DomainInfo.UsersContainer #Creates users in the Users container by default
   $UPNSuffix = "@" + $DomainInfo.DNSRoot
 } #End Set-Environment
 
 Function script:Get-UserData {
   Try {
-    $RandomUsers = Invoke-RestMethod "https://www.randomuser.me/api/?results=$NumUsers&nat=$Nationalities" | Select-Object -ExpandProperty Results
+    $RandomUsers = Invoke-RestMethod "https://www.randomuser.me/api/?results=$NumUsers&amp;nat=$Nationalities" | Select-Object -ExpandProperty Results
   }
   Catch [Exception] {
     Return $_.Exception.Message
   }
 } #End Get-Users
-
-. Set-Environment
-. Get-UserData
 
 Function script:Format-Passwords {
 #Generate passwords to meet default Server 2012 R2 complexity requirements - https://technet.microsoft.com/en-us/library/cc786468(v=ws.10).aspx
@@ -92,6 +89,9 @@ Function script:Format-Passwords {
   }
   Return $Password | ConvertTo-SecureString -AsPlainText -Force #Pass a SecureString to New-ADUser
 } #End Format-Passwords
+
+. Set-Environment
+. Get-UserData
 
 ForEach ($RandomUser in $RandomUsers) {
   $First = $RandomUser.Name.First.Substring(0,1).ToUpper()+$RandomUser.Name.First.Substring(1).ToLower()
@@ -116,7 +116,7 @@ ForEach ($RandomUser in $RandomUsers) {
   "Path" = $UsersOU
   }
 
-  New-ADUser @UserProperties
+  New-ADUser @UserProperties -Credential $Credentials
   $UserPropertiesObj = New-Object PSObject -Property $UserProperties
   $UserPropertiesObj | Add-Member $PlainTextPW
   $RandomUsersArr.Add($UserPropertiesObj) | Out-Null #Add the object to the array
